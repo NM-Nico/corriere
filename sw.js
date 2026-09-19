9c25d756d682a25eba6ace8d7f93222d0e7a71ee
#NICO - NM <nicolagiuntoni@gmail.com>
2026-09-19 23:06:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C='giro-ossigeno-v3';
+const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  const r=e.request; if(r.method!=='GET') return;
+  if(r.mode==='navigate'){e.respondWith(fetch(r).then(res=>{const c=res.clone();caches.open(C).then(x=>x.put('./index.html',c));return res}).catch(()=>caches.match('./index.html')));return}
+  e.respondWith(caches.match(r).then(hit=>hit||fetch(r).then(res=>{if(res.ok||res.type==='opaque'){const c=res.clone();caches.open(C).then(x=>x.put(r,c))}return res})));
+});
