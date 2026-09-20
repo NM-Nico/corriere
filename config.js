4685c912d9f400f5b17e3a7273597f9d5306d9ae
#NICO - NM <nicolagiuntoni@gmail.com>
2026-09-20 14:02:13 +0200

    LOGIN

---
+++ config.js
@@ -0,0 +1,5 @@
+// Dati del progetto Supabase di Medic Air (solo chiave pubblica "publishable")
+window.MEDICAIR_CONFIG = {
+  supabaseUrl: 'https://bnhesngksvnxudxqujdb.supabase.co',
+  supabaseKey: 'sb_publishable_bAf4ne_7QmjVycGSjKMMiA_SFbPCARd'
+};
