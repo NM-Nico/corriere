8cdce1e50a850d64ea7f49960393fea0dc5a6308
#NICO - NM <nicolagiuntoni@gmail.com>
2026-09-20 12:41:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='medic-air-v13';
+const C='medic-air-v14';
 const FILES=['./','./index.html','./manifest.webmanifest','./medicair-192.png','./medicair-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
